3feaad6cc0b832422b3435e0a01b684387db525f
FernandoDevBh <fsffernando2@gmail.com>
2021-08-22 08:33:35 -0300

    Implementação do serviço de carrinho.

---
+++ src/Services/Basket/Basket.API/Configuration/BasketAppSettings.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Basket.API.Configuration
+{
+  public class BasketAppSettings
+  {
+    private readonly IConfiguration _configuration;
+
+    public BasketAppSettings(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    public string CacheSettings { get => _configuration.GetValue<string>("CacheSettings:ConnectionString"); }
+  }
+}
